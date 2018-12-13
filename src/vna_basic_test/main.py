7313fe02fa4c5203d353f4ed6f18bd13fc3e2cd1
Joel Daricou <33722197+joeldrc@users.noreply.github.com>
2018-12-13 15:00:41 +0100

    added icon

--- src/vna_basic_test/main.py
+++ src/vna_basic_test/main.py
@@ -3,6 +3,11 @@ from tkinter import ttk
 
 from gui_interface import User_gui
 
+
 root = tk.Tk()
+try:
+    root.iconbitmap('icon.ico')
+except:
+    print("No icon file")    
 program = User_gui(root)
 root.mainloop()
