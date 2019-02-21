2a84b045c77cc7109fa801464c7518d3bb35aa70
Joel Daricou <33722197+joeldrc@users.noreply.github.com>
2019-02-21 18:33:32 +0100

    code arranged

--- src/multi_thread.py
+++ src/multi_thread.py
@@ -3,10 +3,6 @@
 
 from visa_scpi import *
 import threading
-from tkinter import *
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
 from openpyxl import *
 
 
@@ -101,37 +97,3 @@ class Measure_thread(threading.Thread):
             self.wb.save(file_position)
         except Exception as e:
             print(e)
-
-
-class Progress_bar():
-    # threaded progress bar for tkinter gui
-    def __init__(self, parent, row, columnspan, sticky, padx, pady):
-        self.maximum = 100
-        self.interval = 10
-        self.progressbar = ttk.Progressbar(parent, orient=tk.HORIZONTAL, mode="indeterminate", maximum=self.maximum)
-        self.progressbar.grid(row= row, columnspan=columnspan, sticky=sticky, padx=padx, pady=pady)
-        self.thread = threading.Thread()
-        self.thread.__init__(target=self.progressbar.start(self.interval), args=())
-        self.pb_clear()
-
-    def pb_stop(self):
-        if not self.thread.isAlive():
-            VALUE = self.progressbar["value"]
-            self.progressbar.stop()
-            self.progressbar["value"] = VALUE
-
-    def pb_start(self):
-        if not self.thread.isAlive():
-            VALUE = self.progressbar["value"]
-            self.progressbar.configure(mode="indeterminate", maximum=self.maximum, value=VALUE)
-            self.progressbar.start(self.interval)
-
-    def pb_clear(self):
-        if not self.thread.isAlive():
-            self.progressbar.stop()
-            self.progressbar.configure(mode="determinate", value=0)
-
-    def pb_complete(self):
-        if not self.thread.isAlive():
-            self.progressbar.stop()
-            self.progressbar.configure(mode="determinate", maximum=self.maximum, value=self.maximum)
