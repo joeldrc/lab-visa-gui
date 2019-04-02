76c6b3d418530abfc3f47c7839d66986f5e40095
Joel Daricou <33722197+joeldrc@users.noreply.github.com>
2019-04-02 18:02:31 +0200

    bug fixed

--- src/user_gui/user_gui.py
+++ src/user_gui/user_gui.py
@@ -13,7 +13,7 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1118, 639)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../images/icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("images/icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
