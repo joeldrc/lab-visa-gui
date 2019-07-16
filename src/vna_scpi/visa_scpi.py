a2d6dc0f4d4d58e06bf001fb05f1c24d00d9e8f7
Joel Daricou <33722197+joeldrc@users.noreply.github.com>
2019-07-16 14:24:37 +0200

    code updated (standardization)

--- src/vna_scpi/visa_scpi.py
+++ src/vna_scpi/visa_scpi.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import time
+from time import gmtime, strftime
 import visa
 import pyvisa as visa
 import threading
@@ -9,18 +10,20 @@ import numpy as np
 
 
 # Global variables
-calibration = 0
+calibration = ''
 loading_time = 20 #seconds
 
 
 class Vna_measure(threading.Thread):
-    def __init__(self, address, test_type = 0, folder_name = 'none', test_name = 'none'):
+    def __init__(self, address, test_type = '', folder_name = 'data', test_name = strftime("%d%m%Y_%H%M%S", gmtime())):
         threading.Thread.__init__(self)
 
         self.instrument_address = address
         self.test_type = test_type
+
         self.folder_name = folder_name
         self.test_name = test_name
+
         self.measures = []
         self.instrument_info = ''
         self.data_ready = False
@@ -28,7 +31,7 @@ class Vna_measure(threading.Thread):
         self.start() # Start thread
 
     def run(self):
-        print("Init. visa setup")
+        print("\nInit. visa setup\n")
 
         try:
             if self.instrument_address == "TEST":   # TEST is used for debug
@@ -50,25 +53,23 @@ class Vna_measure(threading.Thread):
                 # Read default directory
                 print(self.vna.query('MMEMory:CDIRectory?'))
 
-                #start measures
-                if self.test_type == 1:
-                    pathname = 'C:\Rohde&schwarz\\Nwa\Automatic_tests\\Feedthrough\\'
-                    typeName = 'Feedthrough_test.zvx'
+                try:
+                    pathname = 'C:\Rohde&schwarz\\Nwa\Automatic_tests\\%s\\' % (self.test_type)
+                    typeName = '%s_test.zvx' % (self.test_type)
 
-                elif self.test_type == 2:
-                    pathname = 'C:\Rohde&schwarz\\Nwa\Automatic_tests\\Pick_up\\'
-                    typeName = 'Pick_up_test.zvx'
+                    if self.test_type > '':
+                        global calibration
+                        if (calibration != self.test_type):
+                            self.load_instrument_state(pathname + typeName)
+                            calibration = self.test_type
 
-                if self.test_type > 0:
-                    global calibration
-                    if (calibration != self.test_type):
-                        self.load_instrument_state(pathname + typeName)
-                        calibration = self.test_type
+                        self.auto_scale_screen()
+                        self.read_data()
+                        self.export_data(pathname + self.folder_name, self.test_name)
+                        print('End measures')
 
-                    self.auto_scale_screen()
-                    self.read_data()
-                    self.export_data(pathname + self.folder_name, self.test_name)
-                    print('End measures')
+                except Exception as e:
+                    print(e)
 
             self.data_ready = True
 
@@ -92,7 +93,8 @@ class Vna_measure(threading.Thread):
             time.sleep(seconds)
 
         # Wait until the command is executed
-        print(self.vna.query('*WAI; *OPC?'))
+        msg = self.vna.query('*WAI; *OPC?')
+        #print(msg)
 
 
 #==============================================================================#
@@ -155,12 +157,12 @@ class Vna_measure(threading.Thread):
 
             # Receive measure
             yData = self.vna.query('CALC1:DATA? FDAT')
-            print(yData)
+            #print(yData)
             self.wait()
 
             # Receive the number of point measured
             xData = self.vna.query('CALC1:DATA:STIM?')
-            print(xData)
+            #print(xData)
             self.wait()
 
             yDataArray = yData.split(",")
@@ -191,7 +193,7 @@ class Vna_measure(threading.Thread):
         self.wait()
         # read all traces from VNA (.csv file)
         self.all_traces = self.vna.query_binary_values("MMEM:DATA? '%s\%s.csv' " % (pathname, fileName), datatype='B', is_big_endian=False, container=bytearray)
-        print(self.all_traces)
+        #print(self.all_traces)
         self.wait()
 
         # save S-Param
@@ -199,7 +201,7 @@ class Vna_measure(threading.Thread):
         self.wait()
         # read S-parameters from VNA (.sp file)
         self.s_parameters = self.vna.query_binary_values("MMEM:DATA? '%s\%s.s2p' " % (pathname, fileName), datatype='B', is_big_endian=False, container=bytearray)
-        print(self.s_parameters)
+        #print(self.s_parameters)
         self.wait()
 
         # save png file
@@ -210,7 +212,7 @@ class Vna_measure(threading.Thread):
         self.wait()
         # read pictures from VNA (.png file)
         self.picture = self.vna.query_binary_values("MMEM:DATA? '%s\%s.png' " % (pathname, fileName), datatype='B', is_big_endian=False, container=bytearray)
-        print(self.picture)
+        #print(self.picture)
         self.wait()
 
 
@@ -219,105 +221,71 @@ class Vna_measure(threading.Thread):
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
-    address = "TCPIP::CFO-MD-BQPVNA1::INSTR" #"TEST"
-    test = Vna_measure(address, 1) #test_type
+    print("Pyton test software 2019\n")
 
-    while test.data_ready == False:
-        print('wait')
-        time.sleep(0.5)
+    while(1):
+        run_script =  input('Continue [y/n](enter to default)?:')
+        if run_script == '':
+            run_script = 'y'   #default
+        print('-> ', run_script)
+        if run_script != 'y':
+            break
 
-    for i in range(len(test.measures)):
-        x, y = test.measures[i]
-        plt.title ("test")
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.plot(x, y)
-        plt.show()
+        address = input('Enter address (enter to default):')
+        if address == '':
+            address = "TCPIP::CFO-MD-BQPVNA1::INSTR"    #default, you can also chose "TEST"
+        print('-> ', address)
 
+        test_name = input('Enter test name (enter to default):')
+        if test_name == '':
+            test_name = 'Feedthrough'   #default
+        print('-> ', test_name)
 
-"""
-# -----------------------------------------------------------
-# read all traces from VNA (.csv file)
-#self.all_traces = self.vna.query("MMEM:DATA? '%s\%s.csv' " % (pathname, fileName))
-#self.all_traces = self.all_traces.replace("\r", "") #remove new row
+        save_files = input('Save files [y/n](enter to default)?:')
+        if save_files == '':
+            save_files = 'n'   #default
+        print('-> ', save_files)
 
-# -----------------------------------------------------------
-# read S-parameters from VNA (.sp file)
-#self.s_parameters = self.vna.query("MMEM:DATA? '%s\%s.s2p' " % (pathname, fileName))
-#self.s_parameters = self.s_parameters.replace("\r", "") #remove new row
+        test = Vna_measure(address, test_name)
 
-# -----------------------------------------------------------
-# read pictures from VNA (.png file)
-#self.vna.write("MMEM:DATA? '%s\%s.png' " % (pathname, fileName))
-#self.picture = self.vna.read_raw()
-#cutting_character = self.picture.find(b'\x89')
-#self.picture = self.picture[cutting_character:] #remove characters up to \x89
+        i = 0
+        while test.data_ready == False:
+            s = str(i) + '%'    # string for output
+            print('wait: ' + s, end='')    # just print and flush
+            # back to the beginning of line
+            print('\r', end='') # use '\r' to go back
+            time.sleep(0.5)
+            i += 1
 
-# -----------------------------------------------------------
-#self.vna.write('*RST') # Reset the instrument
-#self.vna.write('*CLS') # Clear the Error queue
+        for i in range(len(test.measures)):
+            x, y = test.measures[i]
+            plt.title ("test")
+            plt.xlabel("x")
+            plt.ylabel("y")
+            plt.plot(x, y)
+            plt.show()
 
-# Display update ON - switch OFF after debugging
-self.vna.write('SYST:DISP:UPD ON')
+        if save_files == 'y':
 
-# -----------------------------------------------------------
-self.vna.write("CALC1:PAR:DEF 'Trc1', S21")
-self.vna.write('DISP:WIND:STAT ON')
-self.vna.write("DISP:WIND:TRAC1:FEED 'Trc1'")
+            name = strftime("%d%m%Y_%H%M%S", gmtime())
 
-# marker
-self.vna.write('CALC1:MARK ON')
-self.vna.write('CALCulate1:MARKer1:X 2Ghz')
-self.vna.write('CALC1:FORM GDELay')
-time.sleep(1)
-self.vna.write('DISP:WIND:TRAC1:Y:SCAL:AUTO ONCE')
-time.sleep(1)
+            # Save all files received from vna
+            print("Saving files")
 
-# -----------------------------------------------------------
-self.vna.write("CALC1:PAR:DEF 'Trc1', S21")
-self.vna.write('DISP:WIND:STAT ON')
-self.vna.write("DISP:WIND:TRAC1:FEED 'Trc1'")
-# marker
-self.vna.write('CALC1:MARK ON')
-self.vna.write('CALCulate1:MARKer1:X 2Ghz')
-self.vna.write('CALC1:FORM MLOG')
-time.sleep(1)
-self.vna.write('DISP:WIND:TRAC1:Y:SCAL:AUTO ONCE')
-time.sleep(1)
+            # export sp file
+            file = open(name + '.s2p',"wb")
+            file.write(test.s_parameters)
+            file.close()
+            print('File saved')
 
-# -----------------------------------------------------------
-self.vna.write("CALC1:PAR:DEF 'Trc1', S11")
-self.vna.write('DISP:WIND:STAT ON')
-self.vna.write("DISP:WIND:TRAC1:FEED 'Trc1'")
-# marker
-self.vna.write('CALC1:MARK ON')
-self.vna.write('CALCulate1:MARKer1:X 2Ghz')
-self.vna.write('CALC1:FORM SWR')
-time.sleep(1)
-self.vna.write('DISP:WIND:TRAC1:Y:SCAL:AUTO ONCE')
-time.sleep(1)
+            # export csv file
+            file = open(name + '.csv',"wb")
+            file.write(test.all_traces)
+            file.close()
+            print('File saved')
 
-# -----------------------------------------------------------
-self.vna.write("CALC1:PAR:DEF 'Trc1', S22")
-self.vna.write('DISP:WIND:STAT ON')
-self.vna.write("DISP:WIND:TRAC1:FEED 'Trc1'")
-# marker
-self.vna.write('CALC1:MARK ON')
-self.vna.write('CALCulate1:MARKer1:X 2Ghz')
-self.vna.write('CALC1:FORM SWR')
-time.sleep(1)
-self.vna.write('DISP:WIND:TRAC1:Y:SCAL:AUTO ONCE')
-time.sleep(1)
-
-# -----------------------------------------------------------
-self.vna.write("CALC1:PAR:DEF 'Trc1', S11")
-self.vna.write('DISP:WIND:STAT ON')
-self.vna.write("DISP:WIND:TRAC1:FEED 'Trc1'")
-self.vna.write('CALC1:FORM REAL')
-# time domain
-self.vna.write('CALC1:TRAN:TIME:STAT ON')
-self.vna.write('CALC1:TRAN:TIME LPAS; TIME:STIM STEP')
-time.sleep(1)
-self.vna.write('DISP:WIND:TRAC1:Y:SCAL:AUTO ONCE')
-time.sleep(1)
-"""
+            # export png files
+            file = open(name + '.png',"wb")
+            file.write(test.picture)
+            file.close()
+            print('File saved')
